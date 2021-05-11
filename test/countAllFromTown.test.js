7796b3a253071091f34820e1514bb4e1f555b191
loras26 <nyambegugu@gmail.com>
2021-05-11 10:59:00 +0200

    initial commit

--- test/countAllFromTown.test.js
+++ test/countAllFromTown.test.js
@@ -1,5 +1,5 @@
 let assert = require("assert");
-let countAllFromTown = require("../counntAllFromTown");
+let countAllFromTown = require("../countAllFromTown");
 describe('The test for the countAllFromTown function' , function(){
     it('This function return the length 3 when three regNos with the string start "CL" are passed ' , function(){
         assert.equal(3, countAllFromTown("CL 124,CY 567,CL 345, CJ 456,CL 341,'CL'"));
